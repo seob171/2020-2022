181bc1ae5f62abaa0c29426e243ed4a6bf02f91a
seob717 <seob717@netand.co.kr>
2022-05-21 13:40:55 +0900

    feat : 웹팩, 바벨 설정

---
+++ dist/main.js
@@ -0,0 +1,49 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\nfunction component() {\n  var element = document.createElement('div');\n  element.innerHTML = \"hello webpack\";\n  return element;\n}\n\ndocument.body.appendChild(component());//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMuanMiLCJtYXBwaW5ncyI6IjtBQUFBO0FBQ0E7QUFFQTtBQUVBO0FBQ0E7O0FBRUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly96b29tLy4vc3JjL2luZGV4LmpzP2I2MzUiXSwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gY29tcG9uZW50KCl7XG4gICAgY29uc3QgZWxlbWVudCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpXG5cbiAgICBlbGVtZW50LmlubmVySFRNTCA9IGBoZWxsbyB3ZWJwYWNrYFxuXG4gICAgcmV0dXJuIGVsZW1lbnRcbn1cblxuZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChjb21wb25lbnQoKSkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+import path from 'path'
+// import {fileURLToPath} from 'url'
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __dirname = path.resolve()
+
+// const dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const config = {
+    entry:'./src/index.js',
+    output: {
+        filename:'main.js',
+        path:path.resolve(__dirname,'dist'),
+        clean:true
+    },
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                exclude:/(node_modules|pages)/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test:/\.css$/,
+                use:[
+                    {loader:MiniCssExtractPlugin.loader},
+                    {
+                        loader:'css-loader',
+                        options:{import:true}
+                    }
+                ]
+            },
+            {
+                test:/\.png$/,
+                type:'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template:'./index.html'}),
+        new MiniCssExtractPlugin()
+    ],
+    devtool: 'eval-cheap-module-source-map',
+    target:'web',
+    devServer:{
+        // contentBase:path.resolve(dirname,'dist'),
+        // compress:true,
+        static:{
+            directory:path.resolve(__dirname,"dist")
+        },
+        hot:false,
+        historyApiFallback:true,
+        liveReload:true,
+        open:true,
+        port:5500,
+        // watchContentBase:true,
+        // watchOptions:{
+        //     poll:1000,
+        //     ignored:/node_modules/
+        // }
+    },
+    // stats:{
+    //     children:true
+    // }
+}
+
+export default config
