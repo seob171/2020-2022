b498b196f135e72029a178a938027453d9412063
seob717 <seob717@netand.co.kr>
2022-07-16 03:16:41 +0900

    feat : 클래스 캡슐화 이해 및 구현하기

--- encapsulation.ts
+++ encapsulation.ts
@@ -48,5 +48,40 @@
     // const maker = new CoffeeMachine(31)
     const maker = CoffeeMachine.makeMachine(32)
     maker.fillCoffeeBeans(32)
-    maker.coffeeBeans  = -30 // invalid
+    // maker.coffeeBeans  = -30 // invalid
+
+    class User{
+        firstName:string
+        lastName:string
+        fullName:string
+        constructor(firstName:string,lastName:string){
+            this.firstName = firstName
+            this.lastName = lastName
+            this.fullName = `${this.firstName} ${this.lastName}`
+        }
+    }
+    const user = new User('Steve','Jobs')
+    console.log(user.fullName) // Steve Jobs
+    user.firstName = 'seob'
+    console.log(user.fullName) // Steve Jobs
+
+    // firstName을 seob로 수정해도 최초 생성시 fullName이 지정되었기 때문에 변경된 firstName을 적용하지 않는다.
+    // 때문에 getter, setter로 값을 수정하고 적용할 수 있다.
+
+    class Person{
+        get fullName():string{
+            return  `${this.firstName} ${this.lastName}`
+        }
+        set fullName(fullName:string){
+            this.fullName = fullName
+        }
+
+        constructor(private firstName:string,private lastName:string){
+        }
+    }
+    const person = new User('Steve','Jobs')
+    console.log(person.fullName) // Steve Jobs
+    person.firstName = 'Seob'
+    console.log(person.fullName) // Seob Jobs
+
 }
