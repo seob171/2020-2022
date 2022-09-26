00695443aeeaac1e06b64fc6aa5724ebe74bb0d1
seob717 <seob717@netand.co.kr>
2022-09-26 22:35:32 +0900

    feat : page, router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, NotFound } from "./pages/index";
 
 const App: React.FC = () => {
-    return <div>Hello TS</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="*" element={<NotFound />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;
